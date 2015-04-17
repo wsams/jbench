f18c82198e2c6301038adf0f6c96ab876f86e49b
Weldon Sams <wjsams@gmail.com>
2015-04-17 13:53:34 -0400

    test(elapsed): Updated unit tests to include better elapsed time testing.

--- src/test/java/io/zoopaz/jbench/TimerTest.java
+++ src/test/java/io/zoopaz/jbench/TimerTest.java
@@ -3,7 +3,9 @@ package io.zoopaz.jbench;
 import static org.junit.Assert.*;
 import io.zoopaz.jbench.Timer;
 import java.math.BigDecimal; 
+import java.math.RoundingMode;
 import org.junit.Test;
+import org.junit.Ignore;
 import java.lang.Thread;
 
 public class TimerTest {
@@ -45,6 +47,12 @@ public class TimerTest {
         assertNotEquals(start, t.getStart());
     }
 
+    /**
+     * This is difficult to test as it computes time differences. The
+     * best I can come up with right now is to create a timer, sleep
+     * for 2 seconds and ensure that the elapsed amount of time is
+     * greater than 1 second.
+     */
     @Test
     public void test_get_elapsed() throws InterruptedException {
         Timer t = new Timer();
@@ -52,14 +60,66 @@ public class TimerTest {
         assertTrue(t.getElapsed().compareTo(new BigDecimal(1)) == 1);
     }
 
+    /**
+     * This is difficult to test as it computes average time differences. The
+     * best I can come up with right now is to create a timer, sleep for 2 
+     * seconds and ensure that the elapsed amount of time is greater than 1 second.
+     */
     @Test
     public void test_get_average_elapsed() throws InterruptedException {
         Timer t = new Timer();
+        Thread.sleep(2000);
         t.lap();
+        Thread.sleep(2000);
         t.lap();
+        Thread.sleep(2000);
         t.lap();
-        Thread.sleep(4000);
-        assertTrue(t.getElapsed().compareTo(new BigDecimal(1)) == 1);
+        // Testing that the total elapsed time is greater than 5 because
+        // we know it's at least 6 seconds. With rounding this test should
+        // be exactly 6 seconds, but I don't want to assume that.
+        assertTrue(t.getElapsed().compareTo(new BigDecimal(5)) == 1);
+
+        // The average elapsed time should be greater than 1 as each lap
+        // takes 2 seconds.
+        assertTrue(t.getLapAverage().compareTo(new BigDecimal(1)) == 1);
+    }
+
+    /**
+     * This test tries to get the exact elapsed time by rounding but is not
+     * perfect. It's ignored because it adds wait time to the test and is
+     * not perfect.
+     */
+    @Ignore
+    @Test
+    public void test_get_elapsed_exact() throws InterruptedException {
+        Timer t = new Timer();
+        Thread.sleep(1000);
+        t.lap();
+        Thread.sleep(1000);
+        t.lap();
+        Thread.sleep(1000);
+        t.lap();
+        BigDecimal exactElapsed = t.getElapsed().setScale(0, RoundingMode.FLOOR);
+        assertTrue(exactElapsed.compareTo(new BigDecimal(3)) == 0);
+    }
+
+    /**
+     * This test tries to get the exact average elapsed time by rounding but is not
+     * perfect. It's ignored because it adds wait time to the test and is not perfect.
+     */
+    @Ignore
+    @Test
+    public void test_get_average_elapsed_exact() throws InterruptedException {
+        Timer t = new Timer();
+        Thread.sleep(1000);
+        t.lap();
+        Thread.sleep(1000);
+        t.lap();
+        Thread.sleep(1000);
+        t.lap();
+        BigDecimal exactLapAverage = t.getLapAverage().setScale(0, RoundingMode.FLOOR);
+        System.out.println("exact lap average: " + exactLapAverage);
+        assertTrue(exactLapAverage.compareTo(new BigDecimal(1)) == 0 || exactLapAverage.compareTo(new BigDecimal(0)) == 0);
     }
 
 }
